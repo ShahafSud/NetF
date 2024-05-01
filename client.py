3bf2db665c36e075cc9e94f54015193df30a4628
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2024-05-01 12:28:01 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,43 +1,37 @@
-# server.py
+# client.py
 import asyncio
 import tracemalloc
-from pathlib import Path
-from aioquic.asyncio import serve
+from aioquic.asyncio import connect
 from aioquic.quic.configuration import QuicConfiguration
 
 
-async def handle_stream(reader, writer):
-    print("New stream opened.")
-    data = await reader.read(1024)
-    while not reader.at_eof():
-        print(f"Received: {data.decode()}")
+async def run_client(host, port):
+    configuration = QuicConfiguration(is_client=True)
+    configuration.verify_mode = False
+    tracemalloc.start()
+
+    async with connect(host, port, configuration=configuration) as protocol:
+        print("Connecting to server...", end="")
+        await protocol.wait_connected()
+        reader, writer = await protocol.create_stream()
+        print("Connected")
+
+        while not reader.at_eof():
+            text = input("Message: ")
+            writer.write(text.encode())
+            print("Sending to server...", end="")
+            await reader.read(1024)
+
+        print("done")
+        print("Sending eof...", end="")
         writer.write_eof()
-        data = await reader.read(1024)
-    print("Stream closed.")
-
-
-async def run_server(host, port):
-    configuration = QuicConfiguration(is_client=False)
-
-    configuration.load_cert_chain(certfile=Path("certificate.pem"), keyfile=Path("key.pem"))
-
-    def handle_stream_awaited(reader, writer):
-        asyncio.create_task(handle_stream(reader, writer))
-
-    await serve(host, port, configuration=configuration, stream_handler=handle_stream_awaited)
+        protocol.close()
+        await protocol.wait_closed()
+        print("done")
 
 
 def main():
-    tracemalloc.start()
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(run_server('localhost', 4433))
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        loop.close()
-
+    asyncio.run(run_client('localhost', 4433))
     tracemalloc.stop()
 
 

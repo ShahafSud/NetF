ea5447d63256de10f2d8f2e503c34937e2c3fb99
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2024-05-05 16:16:26 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
 import asyncio
 import tracemalloc
-import json
 from pathlib import Path
 from aioquic.asyncio import serve
 from aioquic.quic.configuration import QuicConfiguration
 
 FILE_PATH = "received_file.txt"
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 4096
 
 
-def write_chunks_to_file(packets):
+async def write_chunks_to_file(packets):
     try:
         # Open the file in write mode
-        with open(FILE_PATH, 'w') as file:
+        with open(FILE_PATH, 'wb') as file:
             # Write each chunk to the file
-            for key in range(len(packets)):
-                file.write(packets[key])
+            for packet in packets:
+                file.write(packet)
         print("File created successfully.")
     except IOError as e:
         print(f"Error writing to file: {e}")
@@ -27,66 +26,26 @@ async def close_client(writer, packets):
     await writer.drain()
     print("Stream closed.")
 
-    write_chunks_to_file(packets)
-
-
-async def receive_full_packet(reader):
-    packet_data = b""
-    packet_size = 0
-
-    while True:
-        data_chunk = await reader.read(BUFFER_SIZE - packet_size)
-        if not data_chunk:
-            # End of stream reached
-            return None if packet_size == 0 else packet_data  # Return None if no data received, otherwise return received data
-
-        packet_data += data_chunk
-        packet_size = len(packet_data)
-
-        if packet_size >= BUFFER_SIZE:
-            # Received packet of size equal to or greater than BUFFER_SIZE
-            return packet_data
-
-
-async def decode_json_packet(reader):
-    packet_data = await receive_full_packet(reader)
-    if packet_data is None:
-        return -1, "", "break"
-
-    try:
-        data_json = packet_data.decode('utf-8')
-        data = json.loads(data_json)
-        serial_num = data["serial_number"]
-        packet_data = data["data"]
-
-        if len(packet_data) < BUFFER_SIZE:
-            return serial_num, packet_data, "break"
-
-        return serial_num, packet_data, "continue"
-
-    except json.JSONDecodeError as e:
-        print(f"Error decoding JSON data: {e}")
-        return -1, "", "break"
+    await write_chunks_to_file(packets)
 
 
 async def handle_stream(reader, writer):
-    packets = dict()
+    packets = []
     print("New stream opened.")
-    while True:
-        serial_num, data, command = await decode_json_packet(reader)
+    try:
+        while True:
+            data_chunk = await reader.read(BUFFER_SIZE)
+            if not data_chunk:
+                # End of stream reached
+                break
+            packets.append(data_chunk)
 
-        if serial_num != -1:
-            packets[serial_num] = data
-
-        if command == "break":
-            break
-
-    await close_client(writer, packets)
+    finally:
+        await close_client(writer, packets)
 
 
 async def run_server(host, port):
     configuration = QuicConfiguration(is_client=False)
-
     configuration.load_cert_chain(certfile=Path("certificate.pem"), keyfile=Path("key.pem"))
 
     def handle_stream_awaited(reader, writer):
